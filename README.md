90a46201984ccd46bbe6d5c78b49f1a88cffd633
JesusCotlame <jcotlame@iwa.com.mx>
2024-02-13 12:20:25 -0600

    feat(action): add interface GH context
    Author: JesusCotlame
    Refs: create-gh-actions-upload-file-and-trigger-analysis

--- README.md
+++ README.md
@@ -4,17 +4,17 @@ GitHub Actions for interacting with [Pixee](https://pixee.ai/), providing seamle
 
 ## Actions
 
-### 1. Upload File
+### 1. Composite
+
+The composite action combines the file upload and analysis trigger functionalities into one, streamlining and automating the analysis process within your development workflow.
+
+### 2. Upload File
 
 This action uploads a file to an AWS S3 bucket, simplifying the process of uploading relevant files for further processing or reference.
 
-### 2. Trigger
+### 3. Trigger
 
 This action triggers an analysis of pull requests on GitHub using Pixee. By automatically initiating the analysis in response to specific events, such as the opening of a pull request, this action seamlessly integrates the analysis process into your development workflow.
-
-### 3. Composite
-
-The composite action combines the file upload and analysis trigger functionalities into one, streamlining and automating the analysis process within your development workflow.
 
 ## Examples
 
--- src/util.ts
+++ src/util.ts
@@ -4,7 +4,14 @@ import * as github from '@actions/github';
 type EndpointType = 'upload' | 'trigger'
 const PIXEE_SAMBOX_URL = 'https://d22balbl18.execute-api.us-east-1.amazonaws.com/prod'
 
-export function buildApiUrl(type: EndpointType, url: string, prNumber: number | null, tool?: string) {
+interface GitHubContext {
+    owner: string;
+    repo: string;
+    number: number;
+    sha: string;
+}
+
+export function buildApiUrl(type: EndpointType, url: string, prNumber: number | null, tool?: string): string {
     const customUrl = url ? url : PIXEE_SAMBOX_URL
     const {owner, repo, number, sha} = getGithubContext()
 
@@ -15,9 +22,9 @@ export function buildApiUrl(type: EndpointType, url: string, prNumber: number | 
     return `${customUrl}/analysis-input/${owner}/${repo}/${prNumber ?? number}`
 }
 
-export function getGithubContext() {
-    const {sha, issue: {owner, repo, number}} = github.context
-    return {owner, repo, number, sha}
+export function getGithubContext(): GitHubContext {
+    const { sha, issue: { owner, repo, number } } = github.context;
+    return { owner, repo, number, sha };
 }
 
 export function wrapError(error: unknown): Error {
